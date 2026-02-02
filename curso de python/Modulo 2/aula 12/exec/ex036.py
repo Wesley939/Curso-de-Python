4bfc4cec71423ee44c3f6e9ce44b34db416b559d
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-02 11:11:29 -0300

    esse commit descreve os que cada programa faz

--- curso de python/Modulo 2/aula 12/exec/ex036.py
+++ curso de python/Modulo 2/aula 12/exec/ex036.py
@@ -1,10 +1,10 @@
 """
 
-escreva um programa para aprovar o empréstimo bancario para a compra de uma casa.
+escreva um programa para aprovar o emprestimo bancario para a compra de uma casa.
 O programa vai pergunta o valor da casa, salario do comprador e em quantos anos 
 ele vai pagar
 
-Cacule o valor da prestação mensal sabendo que ela não pode exceder 30% do salario
-ou então o empréstimo se negado.
+Cacule o valor da prestacao mensal sabendo que ela nao pode exceder 30% do salario
+ou entao o emprestimo se negado.
 
 """
