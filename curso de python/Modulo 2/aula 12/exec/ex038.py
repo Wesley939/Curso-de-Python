fd81e075e930a133ba462a930375ba9775525e21
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-02 08:28:11 -0300

    esse commit descreve os que cada programa faz

---
+++ curso de python/Modulo 2/aula 12/exec/ex038.py
@@ -0,0 +1,10 @@
+"""
+
+escreva um programa que leia dois numeros inteiros e compare-se,
+mostrando na tela uma mensagem:
+
+> O primeros valor é Maior
+> O segundo valor é Maior
+> Não existe um valor Maior, os dois são iguais
+
+"""
