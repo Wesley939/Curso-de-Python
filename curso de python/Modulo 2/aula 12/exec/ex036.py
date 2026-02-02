19cf7f5e7b5a850b9898a40a0d63ce04b740715c
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-02 08:12:04 -0300

    inicio do segundo modulo

---
+++ curso de python/Modulo 2/aula 12/exec/ex036.py
@@ -0,0 +1,10 @@
+"""
+
+escreva um programa para aprovar o empréstimo bancario para a compra de uma casa.
+O programa vai pergunta o valor da casa, salario do comprador e em quantos anos 
+ele vai pagar
+
+Cacule o valor da prestação mensal sabendo que ela não pode exceder 30% do salario
+ou então o empréstimo se negado.
+
+"""
--- curso de python/curso_de_python.py
+++ curso de python/curso_de_python.py
@@ -1,1 +1,10 @@
-
+valor1  = 1 
+valor = 2
+if (valor > valor1):
+	print(f"{valor}>{valor1}")
+elif(valor < valor1):
+	print(valor)
+elif(valor <= valor1):
+	print(valor1)
+else:
+	print("testa")
