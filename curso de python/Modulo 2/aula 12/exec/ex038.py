523b44631aa29e9b8a0ce073df360bfc0ed31112
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-03 09:34:46 -0300

    Comparador número

--- curso de python/Modulo 2/aula 12/exec/ex038.py
+++ curso de python/Modulo 2/aula 12/exec/ex038.py
@@ -9,3 +9,12 @@ mostrando na tela uma mensagem:
 > Não existe um valor Maior, os dois são iguais
 
 """
+number1 = int(input('Digite o primerio valor: '))
+number2 = int(input('Digite o segundo valor: '))
+
+if number1 > number2 :
+    print('O primero valor é maior')
+elif number1 == number2 :
+    print('Não existe um valor maior ambos os valores são iguais ')
+else:
+    print('O segundo numero é maior')
