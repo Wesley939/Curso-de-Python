93b412f6549bec3bcc961a9d3eb072812b7eafd5
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-03 09:00:55 -0300

    Conversor numerico

--- curso de python/Modulo 2/aula 12/exec/ex037.py
+++ curso de python/Modulo 2/aula 12/exec/ex037.py
@@ -7,3 +7,16 @@ e peça para o usuário escolher qual será a base de conversão:
 3 -> para hexadecimal
 
 """
+number = int(input('Digite um número inteiro: '))
+print(''' Escolha uma das bases para conversão: 
+[ 1 ] Converter para Binario
+[ 2 ] Converter para Octal
+[ 3 ] converter para Hexadecimal  ''')
+opcao = int(input('Sua opção: '))
+
+if opcao == 1:
+    print(f'{number} convertido para Binario é igual a  {bin(number)[2:]}')
+elif opcao == 2:
+    print(f'{number} Convertido para Octal é igual a {oct(number)[2:]}')
+else:
+    print(f'{number} Convertido para Hexadecimal é igual a {hex(number)[2:]}')
