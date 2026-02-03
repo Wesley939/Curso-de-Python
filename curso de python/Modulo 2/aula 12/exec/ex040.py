2d3e52310bbe5b97143204079d036b4c07297328
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-03 12:10:13 -0300

    calculo da media do aluno

--- curso de python/Modulo 2/aula 12/exec/ex040.py
+++ curso de python/Modulo 2/aula 12/exec/ex040.py
@@ -1,6 +1,6 @@
 """
 
-crie um programa que leia duas notas de um aluno e calcule a sua media, 
+crie um programa que leia duas notas de um aluno e calcule a sua media,
 mostrando uma mensagem final, de acordo com a media atingida:
 
 >media abaixo de 5.0:
@@ -9,7 +9,16 @@ reprovado
 >media entre 5.0 e 6.9:
 recuperação
 
->media 7.0 ou superior: 
+>media 7.0 ou superior:
 aprovado
- 
+
 """
+
+nota1 = float(input("Digite a primeira nota: "))
+print(f"Sua nota foi de {media:.2f} ")
+if media >= 7.0:
+    print("você foi \033[1;4;32mAprovado\033[m")
+elif media >= 5.0:
+    print("você ficou de \033[1;4;34mRECUPERAÇÃO\033[m")
+else:
+    print("você foi \033[1;4;31mREPROVADO\033[m")
