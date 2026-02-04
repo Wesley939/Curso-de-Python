b32158d525f507fc93e51c6afb95b87f15ede27d
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-04 09:44:34 -0300

    Verifica que tipo de trangulo que é

--- curso de python/Modulo 2/aula 12/exec/ex042.py
+++ curso de python/Modulo 2/aula 12/exec/ex042.py
@@ -8,3 +8,14 @@ acrescentando o recurso de mostrar que tipo de Triânguloss sera formado.
 > ESCALENO: todos os landos sao diferentes
 
 """
+
+a = int(input('Digite o primeiro lado do triangulo: '))
+b = int(input('Digite o segundo lado do triangulo: '))
+c = int(input('Digite o terceiro lado do triangulo: '))
+
+if b == a and b == c:
+    print('isso forma um triangulo EQUILATERO')
+elif c == a or c == b:
+    print('Dois lados de um triangulo formam um triangulo ISÓSCELES')    
+else:
+    print('Todos os lados do triangulo sao diferentes e isso forma um triangulo ESCALENO ')
