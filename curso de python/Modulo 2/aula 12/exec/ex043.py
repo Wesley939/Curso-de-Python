04e0870c625801897c39cf64d62c1ba62ee89975
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-06 08:31:16 -0300

    Calculo do seu IMC

--- curso de python/Modulo 2/aula 12/exec/ex043.py
+++ curso de python/Modulo 2/aula 12/exec/ex043.py
@@ -14,3 +14,20 @@ seu status de acordo com a tabela abaixo:
 > acima de 40
 	Obesidade Mórbida
 """
+altura = float(input('Qual sua altura: '))
+peso = float(input('Qual o seu peso: '))
+
+imc = peso / (altura ** 2)
+
+print(f'seu IMC é {imc:.2f}')
+
+if imc <= 18.5:
+	print('Abaixo do peso')
+elif imc <= 25:
+	print('Peso ideal')
+elif imc <= 30:
+	print('Sobre peso')
+elif imc <= 40:
+	print('Obesidade')
+else:
+	print('Obesidade Mórbida')
