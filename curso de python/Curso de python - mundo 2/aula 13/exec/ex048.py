e389eacd616f030329498bcff106450ba4254ad4
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-07 14:01:47 -0300

    Esse programa soma todos os numeros impares que são multiplos por 3

--- curso de python/Curso de python - mundo 2/aula 13/exec/ex048.py
+++ curso de python/Curso de python - mundo 2/aula 13/exec/ex048.py
@@ -1,3 +1,8 @@
-# Faça um programa que calcule a soma entre todos 
-# os NÚMEROS IMPARES que são múltiplos de três e que 
+# Faça um programa que calcule a soma entre todos
+# os NÚMEROS IMPARES que são múltiplos de três e que
 # se encontram no intervalo de 1 até 500
+
+for i in range(1, 501):
+    if i % 2 == 1 and i % 3 == 0:
+        soma = i * 3
+        print(f'A soma dos impares multiplos de 3 entre 1 até 500: {soma}')
