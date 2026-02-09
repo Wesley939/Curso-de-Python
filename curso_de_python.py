f2590d3541a00cad95ebb9230d5daf01ed4d0393
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-08 21:09:45 -0300

    Atualização de arquivos

--- curso_de_python.py
+++ curso_de_python.py
@@ -1,10 +1,14 @@
-valor1  = 1 
-valor = 2
-if (valor > valor1):
-	print(f"{valor}>{valor1}")
-elif(valor < valor1):
-	print(valor)
-elif(valor <= valor1):
-	print(valor1)
-else:
-	print("testa")
+# valor1  = 1
+# valor = 2
+# if (valor > valor1):
+# 	print(f"{valor}>{valor1}")
+# elif(valor < valor1):
+# 	print(valor)
+# elif(valor <= valor1):
+# 	print(valor1)
+# else:
+# 	print("testa")
+
+n = 'sim'
+while n != 'dim':
+    n = str(input("Digite sim para finalizar: "))
