548a57411dbde8f3bc1dd03276790734cb145221
Wesley939 <wesleyassisdelvalle08@gmail.com>
2026-02-09 09:52:37 -0300

    descrição dos exercicios

--- curso de python/Curso de python - mundo 2/aula 14/ex057.py
+++ curso de python/Curso de python - mundo 2/aula 14/ex057.py
@@ -1,18 +1,3 @@
 # Faça um programa que leia o SEXO  de uma pessoa, mas só aceite os valores ´M´ ou ´F´.
 # Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
-sexo = str(input("Informe seu sexo: [M/F]")).upper()
-x = sexo
-
-print(x)
-
-while x != 'M' and x != 'F':
-    teste = str(input("Dados inválidos. Por favor, informe seu sexo: ")).upper()
-    x = teste
-    print(f"foi digitado {x}")
-
-
-# nb = ''
-# while nb != 'F':
-#     n = str(input('Digite ´F´ para finalizar: ')).upper()
-#     nb = n
