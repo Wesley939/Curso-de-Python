909eed859c58635cc33fd01c47ca5c158993bb28
wesley assis <83578487+Wesley939@users.noreply.github.com>
2026-02-10 20:02:29 -0300

    exercícios 58 finalizado

--- curso de python/Curso de python - mundo 2/aula 14/ex058.py
+++ curso de python/Curso de python - mundo 2/aula 14/ex058.py
@@ -1,3 +1,26 @@
 #Melhore o jogo do DESAFIO 028 onde o computador vai ´´´pensar´´ em um 
 #NÚMERO ENTRE 0 E 10. Só que agora o jogador vai tentar adivinhar até acertar,
 #mostrando no final quantos palpites foram necessários para vencer.
+
+
+
+
+
+
+from random import randint
+
+print("sou seu computador")
+print("acabei de pensar em um numero emtre 0 e 10")
+print("Sera que você consegue adivinhar")
+
+user = int(input("Qual o seu palpite: "))
+
+computer = randint(0,10)
+counter = 0
+
+while user != computer:
+    print("Menos... Tente novamente mais uma vez.")
+    user = int(input("Qual é seu palpite: "))
+    counter += 1
+
+print(f"acertou com {counter} tentativas. Parabéns. ")   
