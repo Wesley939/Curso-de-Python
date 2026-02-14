b0cea1aa81e76cf50397af8e9de3c36c3ef649bf
kakaroto <kakaroto@wesley.localdomain>
2026-02-14 08:13:45 -0300

    calculo do fatorial

--- curso de python/Curso de python - mundo 2/aula 14/ex060.py
+++ curso de python/Curso de python - mundo 2/aula 14/ex060.py
@@ -1,12 +1,20 @@
-#Faça um programa que leia um número qualquer e mostre o seu fatorial 
+# Faça um programa que leia um número qualquer e mostre o seu fatorial
 
-#Ex:
-#5! = 1x2x3x4x5 = 120
+# Ex:
+# 5! = 1x2x3x4x5 = 120
 
-#number = int(input("Digite o seu fatorial: "))
+# number = int(input("Digite o seu fatorial: "))
 
-fatorial = 1 
-counter =1
-while counter <= 5:
-    print(counter)
-    counter += 1
+number = int(input("Digite o seu fatorial: "))
+fatorial = 1
+print(f"{number}! = ", end="")
+while number > 0:
+    fatorial *= number
+    print(number, end= '')
+    if number > 1:
+        print('', end=" x ")
+    number -= 1
+
+# print()
+
+print(f" = {fatorial}")
